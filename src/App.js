f00e30256c4f3a29c6657b46deb5c32c6210734f
Vasyl20 <57089606+Vasyl20@users.noreply.github.com>
2021-03-17 19:07:10 +0200

    Starting project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component, Fregment } from 'react';
+import './App.css';
+
+function App() {
+  return (
+       <Fregment>
+         
+       </Fregment>
+  )
+}
+
+export default App;
